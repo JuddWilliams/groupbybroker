a9ba15cb9c8724647a5ffc6639f18720ad9c6224
JuddWilliams <juddsurfs@gmail.com>
2025-02-24 21:17:14 -0500

    Overwrite existing project with new project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDJWa3lnA9h1TmOZCvSsp2S77qWJjzkDjA",
+    authDomain: "groupbybroker.firebaseapp.com",
+    databaseURL: "https://groupbybroker-default-rtdb.firebaseio.com",
+    projectId: "groupbybroker",
+    storageBucket: "groupbybroker.firebasestorage.app",
+    messagingSenderId: "367280671691",
+    appId: "1:367280671691:web:6c50167d5a7d1f98a6c44e",
+     measurementId: "G-EVDZ08C41E"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDJWa3lnA9h1TmOZCvSsp2S77qWJjzkDjA",
+    authDomain: "groupbybroker.firebaseapp.com",
+    databaseURL: "https://groupbybroker-default-rtdb.firebaseio.com",
+    projectId: "groupbybroker",
+    storageBucket: "groupbybroker.firebasestorage.app",
+    messagingSenderId: "367280671691",
+    appId: "1:367280671691:web:6c50167d5a7d1f98a6c44e",
+     measurementId: "G-EVDZ08C41E"
+  }
 };
 
 /*
