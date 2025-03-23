bfde37e9afc769ad88b67237bfb575729cb7367c
Judd Williams <juddsurfs@gmail.com>
2025-03-23 16:58:55 -0400

    ..

--- src/app/tabAbout/tabAbout.page.ts
+++ src/app/tabAbout/tabAbout.page.ts
@@ -9,6 +9,7 @@ import { LocationService } from '../services/location.service';
 })
 export class TabAboutPage implements OnInit {
   isPopupOpen = false; // State to control the popup visibility
+  buttonText: string = 'Note: We were unable to determine your location.'; // Default button text
 
   constructor(private locationService: LocationService) {}
 
@@ -35,7 +36,11 @@ export class TabAboutPage implements OnInit {
       console.log(`Postal Code: ${postalCode}`);
 
       if (postalCode.startsWith('322')) {
+        this.buttonText = `Good news, based on your location it's free for your postal code ${postalCode}`; // Update button text
         this.locationService.showFreeAlert();
+      }
+      else{
+        this.buttonText = "Note: We were unable to determine your location."; // Update button text
       }
     }
   }
