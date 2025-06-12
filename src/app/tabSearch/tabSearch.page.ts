5584999eed2081e9f011f77e3b4935c7a6192bc3
Judd Williams <juddsurfs@gmail.com>
2025-06-12 10:35:23 -0400

    working popup

--- src/app/tabSearch/tabSearch.page.ts
+++ src/app/tabSearch/tabSearch.page.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { AlertController, AlertInput, IonInput, ToastController, Platform, ItemReorderEventDetail, LoadingController } from '@ionic/angular';
+import {
+  AlertController,
+  AlertInput,
+  IonInput,
+  ToastController,
+  Platform,
+  ItemReorderEventDetail,
+  LoadingController,
+} from '@ionic/angular';
 import { Geolocation } from '@capacitor/geolocation';
 import { LocationService } from '../services/location.service';
 import { ContractorListingsService } from '../services/contractor-listings.service';
@@ -18,47 +26,44 @@ import { environment } from 'src/environments/environment';
   standalone: false,
 })
 export class TabSearchPage implements OnInit {
-
   @ViewChild('findRadiusInput', { static: false }) findRadiusInput!: IonInput;
   @ViewChild(MapInfoWindow) infoWindow!: MapInfoWindow;
   @ViewChild('googleMap', { static: false }) map!: GoogleMap;
-  
-  mapOptions: google.maps.MapOptions = {    
-  };
-  
+
+  mapOptions: google.maps.MapOptions = {};
+
   isPopupOpen = false;
-  findRadiusForUI: number = 15;// so its not updated as user types... 
+  findRadiusForUI: number = 15; // so its not updated as user types...
 
   targetAddress: Address = { street: '', city: '', state: '', postalCode: '32225' };
   //userAddedMarker: google.maps.LatLngLiteral | null = null;
-  withinRangeContractorListings: {  contractorListing: ContractorListing, location: google.maps.LatLngLiteral }[] = [];
+  withinRangeContractorListings: { contractorListing: ContractorListing; location: google.maps.LatLngLiteral }[] = [];
   contractorListings: ContractorListing[] = [];
 
   private radiusChange$ = new Subject<number>();
   private boundsChange$ = new Subject<void>();
   flickerOverlay = true;
 
-
-  selectedIndustry: string = 'All'; //'All'; //'Lawn care'; 
+  selectedIndustry: string = 'All'; //'All'; //'Lawn care';
   errorMessage: string | null = null;
   isSmallViewport: boolean = false; // Flag to track if the viewport is small
-  numberOfContractorsInArea: number = 5; 
+  numberOfContractorsInArea: number = 5;
   readonly numberOfContractorsInAreaThreshold: number = 5;
-  locationNote: string = ''; 
-  
+  locationNote: string = '';
+
   optionForSale: boolean = true;
   optionTrade: boolean = true;
   optionCover: boolean = true;
-  optionOutForBid: boolean = true;  
-  
+  optionOutForBid: boolean = true;
+
   items = ['Overall', 'Nearest me', 'Popularity by Area', 'Cost', 'Quality', 'Dependability', 'Professionalism'];
   //sorting: string = 'useAi'; // Default sorting option
-  sortingValue: string = 'useAi'; // Default selected value  
-  optionValue: string[] = ['Out for bid','For Sale','Trade','Cover']; // Default selected values for options
+  sortingValue: string = 'useAi'; // Default selected value
+  optionValue: string[] = ['Out for bid', 'For Sale', 'Trade', 'Cover']; // Default selected values for options
   selectedAddress: any = null;
 
   currentContractorListing: any | undefined;
-  
+
   // Custom icons for markers
   targetIcon = {
     url: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png', // Red marker for the target
@@ -80,29 +85,29 @@ export class TabSearchPage implements OnInit {
       handler: (data: any) => {
         if (data.zipCode && data.zipCode.length === 5) {
           //this.targetAddress = data.streetAddress ? `${data.streetAddress}, ${data.zipCode}` : `${data.zipCode}`;
-          // during conversion to object, i did this. may have broken. 
+          // during conversion to object, i did this. may have broken.
           this.targetAddress = {
             street: data.streetAddress || '',
             city: '',
             state: '',
-            postalCode: data.zipCode
+            postalCode: data.zipCode,
           };
-          this.refreshMap(); 
+          this.refreshMap();
           return true; // Allow the alert to be dismissed
         } else {
           // Show an error message if the zip code is not valid
           alert('Please enter a valid 5-digit zip code.');
           return false; // Prevent the alert from being dismissed
         }
-      }
-    }
+      },
+    },
   ];
 
   public alertInputs: AlertInput[] = [
     {
       name: 'streetAddress',
       placeholder: 'Street address   eg. 1600 Pennsylvania Avenue NW',
-      type: 'text'
+      type: 'text',
     },
     {
       name: 'zipCode',
@@ -123,17 +128,15 @@ export class TabSearchPage implements OnInit {
     private locationService: LocationService,
     private contractorListingsService: ContractorListingsService,
     private router: Router, // add this
-    private loadingController: LoadingController,
+    private loadingController: LoadingController
   ) {}
-  
+
   async ngOnInit() {
     this.platFormReady();
 
-    this.boundsChange$
-      .pipe(debounceTime(800))
-      .subscribe(() => {
-        this.checkAddressesWithinRange(); // this is called on  google map init ;-)  so we don't need to call on our page init. 
-      });
+    this.boundsChange$.pipe(debounceTime(800)).subscribe(() => {
+      this.checkAddressesWithinRange(); // this is called on  google map init ;-)  so we don't need to call on our page init.
+    });
 
     // Flicker for 5 seconds, then stop
     setTimeout(() => {
@@ -144,27 +147,26 @@ export class TabSearchPage implements OnInit {
     setTimeout(() => {
       this.showParticipatePopup();
     }, 60000);
-     
-    window.addEventListener('resize', () => this.checkViewportSize());
 
+    window.addEventListener('resize', () => this.checkViewportSize());
   }
 
   async platFormReady() {
     await this.platform.ready();
     this.checkViewportSize();
 
-    // display map in user's area if available. 
+    // display map in user's area if available.
     //
     const coords = await this.getUserLocationAndCheckPostalCode();
-    
+
     if (coords) {
-      this.refreshMap(coords); 
+      this.refreshMap(coords);
     } else {
       this.refreshMap();
     }
   }
 
-  refreshMap(coords?: { lat: number, lng: number }) {
+  refreshMap(coords?: { lat: number; lng: number }) {
     // Use passed coordinates if available, otherwise default to Jacksonville, Fl. (lat: 30.3322, lng: -81.6557)
     const center = coords ? coords : { lat: 30.3322, lng: -81.6557 };
     console.log('Refreshing map with center:', center);
@@ -175,30 +177,47 @@ export class TabSearchPage implements OnInit {
     };
   }
 
-  getStaticMapUrl(address: { street: string, city: string, state: string, postalCode: string }): string {
-    const apiKey =  environment.googleMapsApiKey; // Replace with your actual API key
+  satelliteZoom = 19;
+
+  getStaticMapUrl(
+    address: { street: string; city: string; state: string; postalCode: string },
+    mapType: string = 'roadmap',
+    zoom: number = this.satelliteZoom
+  ): string {
+    const apiKey = environment.googleMapsApiKey;
     const fullAddress = `${address.street}, ${address.city}, ${address.state} ${address.postalCode}`;
-    console.log('Address for static map:', fullAddress);
     const encodedAddress = encodeURIComponent(fullAddress);
-    console.log('Encoded address for static map:', encodedAddress);
-    let value = `https://maps.googleapis.com/maps/api/staticmap?center=${encodedAddress}&zoom=16&size=350x200&maptype=roadmap&markers=color:red%7C${encodedAddress}&key=${apiKey}`;  
-    console.log('Static map URL:', value);
-    return value;
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${encodedAddress}&zoom=${zoom}&size=350x200&maptype=${mapType}&markers=color:red%7C${encodedAddress}&key=${apiKey}`;
   }
-  
+  increaseSatelliteZoom() {
+    this.satelliteZoom = Math.min(this.satelliteZoom + 1, 21);
+  }
+
+  decreaseSatelliteZoom() {
+    this.satelliteZoom = Math.max(this.satelliteZoom - 1, 16);
+  }
+
+  streetViewHeading = 0; // default north
+
+  getStreetViewUrl(location: google.maps.LatLngLiteral): string {
+    const apiKey = environment.googleMapsApiKey;
+    const { lat, lng } = location;
+    return `https://maps.googleapis.com/maps/api/streetview?size=350x200&location=${lat},${lng}&fov=80&heading=${this.streetViewHeading}&pitch=0&key=${apiKey}`;
+  }
+
   onMarkerClick(contractorListing: any): void {
-    // this.selectedAddress = contractorListing;        
+    // this.selectedAddress = contractorListing;
     // this.infoWindow.open();
     // alert('Marker clicked: ' + selectedAddress.contractorListing.address.street);
-    //console.log('Opening popup for:', contractorListing);   
+    //console.log('Opening popup for:', contractorListing);
     this.currentContractorListing = contractorListing;
-    console.log('Opening popup for currentContractorListing:', this.currentContractorListing);    
+    console.log('Opening popup for currentContractorListing:', this.currentContractorListing);
     console.log('Opening popup for ?:', this.currentContractorListing?.contractorListing?.address?.postalCode);
     this.isPopupOpen = true; // Open the popup
   }
 
-  openPopup(Obj?: { contractorListing: ContractorListing, location: google.maps.LatLngLiteral }) {
-    console.log('Opening popup for:', Obj);    
+  openPopup(Obj?: { contractorListing: ContractorListing; location: google.maps.LatLngLiteral }) {
+    console.log('Opening popup for:', Obj);
     this.isPopupOpen = true; // Open the popup
   }
 
@@ -207,12 +226,10 @@ export class TabSearchPage implements OnInit {
   }
 
   selectRadio(value: string): void {
-    this.sortingValue = value;    
+    this.sortingValue = value;
   }
 
-  selectCheckBox(value: string): void {   
-    
-    
+  selectCheckBox(value: string): void {
     const selected: string[] = [];
     if (this.optionOutForBid) selected.push('Out for bid');
     if (this.optionForSale) selected.push('For Sale');
@@ -227,48 +244,48 @@ export class TabSearchPage implements OnInit {
     this.checkAddressesWithinRange();
   }
 
-  async getUserLocationAndCheckPostalCode(): Promise<{ lat: number, lng: number } | null> {
-    const location = await this.locationService.getUserLocation();    
+  async getUserLocationAndCheckPostalCode(): Promise<{ lat: number; lng: number } | null> {
+    const location = await this.locationService.getUserLocation();
 
-    if (location && typeof location.latitude === 'number' && typeof location.longitude === 'number') {      
+    if (location && typeof location.latitude === 'number' && typeof location.longitude === 'number') {
       return { lat: location.latitude, lng: location.longitude };
     }
     return null;
   }
 
-   async presentToast(message: string, color: string = 'danger', duration: number = 3000, position: 'top' | 'bottom' = 'top') {
-    const toast = await this.toastController.create({    
+  async presentToast(message: string, color: string = 'danger', duration: number = 3000, position: 'top' | 'bottom' = 'top') {
+    const toast = await this.toastController.create({
       message: message,
       duration: duration,
       position: position,
-      color: color
+      color: color,
     });
     await toast.present();
   }
 
-    async showParticipatePopup() {
+  async showParticipatePopup() {
     const alert = await this.alertController.create({
       header: 'Join the Community',
       message: 'As a free service you have nothing to loose.  Simple and easy to use.',
       buttons: [
         {
           text: 'maybe later',
-          role: 'cancel'
+          role: 'cancel',
         },
         {
           text: 'participate for free',
           handler: () => {
             this.router.navigate(['/tabs/tabDashboard']); // redirect to tabAbout
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
     await alert.present();
   }
 
   checkViewportSize() {
     const width = window.innerWidth;
-    const height = window.innerHeight;    
+    const height = window.innerHeight;
     this.isSmallViewport = width < 600; // Example: Treat viewports smaller than 600px as "small"
     // if (this.isSmallViewport) {
     //   console.log('Rendering for a small viewport');
@@ -281,36 +298,40 @@ export class TabSearchPage implements OnInit {
 
   geocodeAddress(address: Address, callback: (location: google.maps.LatLngLiteral) => void) {
     const geocoder = new google.maps.Geocoder();
-        
-    geocoder.geocode( 
-    {
-      componentRestrictions: {
-        route: address.street, // Street name
-        // cant be empty .. so o remmoved for now until i improve dlg. 
-        postalCode: address.postalCode, // ZIP code
-        country: 'US', // Restrict to the United States (optional)
+
+    geocoder.geocode(
+      {
+        componentRestrictions: {
+          route: address.street, // Street name
+          // cant be empty .. so o remmoved for now until i improve dlg.
+          postalCode: address.postalCode, // ZIP code
+          country: 'US', // Restrict to the United States (optional)
+        },
       },
-    },       
-    (results, status) => {
-      if (status === google.maps.GeocoderStatus.OK && results && results[0]) {        
-        const location = results[0].geometry.location;        
-        const lat = location.lat();
-        const lng = location.lng();
-        callback({ lat, lng });
-        this.errorMessage = null; // Clear any previous error message
-      } else {
-        console.error('Geocode was not successful for the following reason: ' + status);
-        this.presentToast('Geocode was not successful for the following reason: ' + status);
+      (results, status) => {
+        if (status === google.maps.GeocoderStatus.OK && results && results[0]) {
+          const location = results[0].geometry.location;
+          const lat = location.lat();
+          const lng = location.lng();
+          callback({ lat, lng });
+          this.errorMessage = null; // Clear any previous error message
+        } else {
+          console.error('Geocode was not successful for the following reason: ' + status);
+          this.presentToast('Geocode was not successful for the following reason: ' + status);
+        }
       }
-    });
+    );
   }
-  
-  getLatLng(addressObj: { contractorListing: ContractorListing, location: google.maps.LatLngLiteral }): google.maps.LatLngLiteral {
+
+  getLatLng(addressObj: {
+    contractorListing: ContractorListing;
+    location: google.maps.LatLngLiteral;
+  }): google.maps.LatLngLiteral {
     return addressObj.location;
   }
 
   selectText() {
-    this.findRadiusInput.getInputElement().then(input => input.select());
+    this.findRadiusInput.getInputElement().then((input) => input.select());
   }
 
   // Returns zoom level so that the given radius (in miles) fits in the map view
@@ -332,16 +353,15 @@ export class TabSearchPage implements OnInit {
     this.radiusChange$.next(value);
   }
 
-  onMapBoundsChanged() {    
-    this.boundsChange$.next();    
+  onMapBoundsChanged() {
+    this.boundsChange$.next();
   }
 
   async checkAddressesWithinRange() {
-
     const loading = await this.loadingController.create({
       message: 'Loading map data...',
       spinner: 'crescent', // or 'bubbles', 'dots', etc.
-      backdropDismiss: false
+      backdropDismiss: false,
     });
     await loading.present();
 
@@ -361,7 +381,13 @@ export class TabSearchPage implements OnInit {
     try {
       // fetch contractor listings as a promise
       const responseContractorListings = await firstValueFrom(
-        this.contractorListingsService.ContractorListings(undefined, undefined, undefined, this.selectedIndustry, this.optionValue)
+        this.contractorListingsService.ContractorListings(
+          undefined,
+          undefined,
+          undefined,
+          this.selectedIndustry,
+          this.optionValue
+        )
       );
 
       // map and filter in one step
@@ -397,33 +423,27 @@ export class TabSearchPage implements OnInit {
 
       // optional: delay before showing resolve(), then toast
       await new Promise((resolve) => setTimeout(resolve, 500));
-      
+
       // sort by type (alphabetically, case-insensitive)
-      this.withinRangeContractorListings.sort((a, b) => 
+      this.withinRangeContractorListings.sort((a, b) =>
         a.contractorListing.type.localeCompare(b.contractorListing.type, undefined, { sensitivity: 'base' })
       );
-      
+
       if (this.withinRangeContractorListings.length === 0) {
         this.presentToast('No listings found. Zoom out or try a different area.', 'warning', 3000);
       }
       // else {
       //   this.presentToast(`Found ${this.withinRangeContractorListings.length} contractor listings within specified area.`, 'success', 1000);
       // }
-
     } catch (error) {
       console.error('ContractorListings Error:', error);
       this.presentToast('Error loading contractor listings.', 'danger', 3000);
-    }
-    finally {
+    } finally {
       await loading.dismiss();
     }
   }
 
-  isWithinBounds(lat: number, lng: number, bounds: google.maps.LatLngBounds): boolean 
-  {
+  isWithinBounds(lat: number, lng: number, bounds: google.maps.LatLngBounds): boolean {
     return bounds.contains(new google.maps.LatLng(lat, lng));
   }
-
 }
-
-
